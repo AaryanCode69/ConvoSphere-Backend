7dc7154a8abbc27db92992620e146fdc1ca9dc7f
AaryanCode69 <aaryanupadhyay68@gmail.com>
2025-12-09 23:35:56 +0530

    Initial Commit

---
+++ src/test/java/com/example/convospherebackend/ConvoSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convospherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvoSphereBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
